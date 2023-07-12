a0df1e0c1d8582a1eb9834958268efa4dfc52f92
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-11 21:44:04 -0700

    Add event listeners to entire grid to change background color of grid when moused over

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector("#container");
 
-
-function createGrid() {
-    for (let i = 1; i <= 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        container.appendChild(div);
-    }
+for (let i = 1; i <= 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    container.appendChild(div);
 }
-createGrid();
+
+const gridBoxes = document.querySelectorAll('.grid');
+console.log(gridBoxes);
+gridBoxes.forEach(grid => {
+    grid.addEventListener('mouseover', (e) => {
+        e.target.style.background = 'black';
+    })
+})
