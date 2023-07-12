daeac1cfaa33f791b153f201c74c6dd08eb0bbb3
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-12 08:25:29 -0700

    Added functionality to both buttons to clear and create grids of varying sizes

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const container = document.querySelector("#container");
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
+const createGrid = (size) => {
+    if (size > 100) {
+        alert('Too big!');
+        return console.error('That\'s too big bro');
+    }
+    for (let i = 1; i <= size ** 2; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.height = `${(800 / size) - 2}px`;
+        grid.style.width = `${(800 / size) - 2}px`;
+        grid.addEventListener('mouseover', (e) => e.target.style.background = 'black');
+        container.appendChild(grid);
+    }
 }
-
-const gridBoxes = document.querySelectorAll('.grid');
-gridBoxes.forEach(grid => {
-    grid.addEventListener('mouseover', (event) => {
-        event.target.style.background = 'black';
-    })
-})
+const clearGrid = () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(16);
+}
+createGrid(16);
 
 const sizeBtn = document.querySelector('#size');
-sizeBtn.addEventListener('click', (event) => {
-    prompt('What size do you want?', )
+sizeBtn.addEventListener('click', () => {
+    const response = prompt('What size do you want?');
+    if (response !== null) {
+        clearGrid();
+        createGrid(16);
+    }
 })
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', clearGrid);
