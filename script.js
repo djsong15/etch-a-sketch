444ab56a463c693e380ae7defa13fd87a80c4156
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-13 21:55:35 -0700

    Created event listener to change color mode via dropdown

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector("#container");
 const createGrid = (size) => {
     if (size > 100) {
-        alert('Too big!');
-        return console.error('That\'s too big bro');
+        alert('Too big! Gonna give you 16 x 16');
+        size = 16;
+        // return console.error('That\'s too big bro');
     }
     for (let i = 1; i <= size ** 2; i++) {
         const grid = document.createElement('div');
@@ -17,7 +18,6 @@ const clearGrid = () => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    createGrid(16);
 }
 createGrid(16);
 
@@ -26,9 +26,32 @@ sizeBtn.addEventListener('click', () => {
     const response = prompt('What size do you want?');
     if (response !== null) {
         clearGrid();
-        createGrid(16);
+        createGrid(response);
+    }
+});
+
+const colorMode = document.querySelector('#color-modes');
+colorMode.addEventListener('change', (event) => {
+    console.log(colorMode.value);
+    // let color;
+    // if (colorMode.value === 'black') color = 'black';
+    // else if (colorMode.value === 'random') color = Math.floor(Math.random()*16777215).toString(16);
+    // else color = 'white';
+
+    let currentGridChild = container.firstChild;
+    while (currentGridChild !== container.lastChild) {
+        console.log('change');
+        currentGridChild.addEventListener('mouseover', (e) => {
+            e.target.style.background = colorMode.value === 'black' ? 'black'
+                                        : colorMode.value === 'random' ? ('#' + Math.floor(Math.random()*16777215).toString(16))
+                                        : 'white';
+        });
+        currentGridChild = currentGridChild.nextSibling;
     }
 })
 
 const clearBtn = document.querySelector('#clear');
-clearBtn.addEventListener('click', clearGrid);
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+    createGrid(16);
+});
